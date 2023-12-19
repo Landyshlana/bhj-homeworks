b755ff2906472ec15d1b2e146a41b5f196375d66
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2023-12-19 22:24:30 +0300

    вторая попытка

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,9 +1,9 @@
 const reveal = Array.from(document.querySelectorAll('.reveal'));
 window.addEventListener('scroll', function() {
-    for (let i = 0; i < reveal.length; i++) {
-        let poz = reveal[i].getBoundingClientRect().top;
-        if (poz > 0) {
-            reveal[i].classList.add('reveal_active');
-        }
-    }
+	for (let i = 0; i < reveal.length; i++) {
+		let poz = reveal[i].getBoundingClientRect();
+		if (poz.top > 0 && poz.bottom < window.innerHeight) {
+			reveal[i].classList.add('reveal_active');
+		}
+	}
 });
