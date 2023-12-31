76f72220ab73f7bac0bc075794f2a48f18efe027
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2023-12-31 08:12:27 +0300

    подсказка 2 попытка

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,12 +1,18 @@
-const hastooltip = document.querySelector(".has-tooltip");
-let title = hastooltip.getAttribute("title");
-let tooltip = document.createElement("div");
-tooltip.setAttribute("class", "tooltip");
-tooltip.textContent = title;
+let hastooltips = document.querySelectorAll(".has-tooltip");
+hastooltips.forEach(hastooltip => {
+	let title = hastooltip.getAttribute("title");
+	let tooltip = document.createElement("div");
+	tooltip.setAttribute("class", "tooltip");
+	tooltip.textContent = title;
+	hastooltip.addEventListener("click", event => {
+		tooltip.classList.toggle("tooltip_active");
+		let positiontop = event.target.getBoundingClientRect().top; 
+		let positionleft = event.target.getBoundingClientRect().left;
+		let height = event.target.getBoundingClientRect().height;
 
-hastooltip.insertAdjacentElement("afterend", tooltip);
-
-hastooltip.addEventListener("click", (event) => {
-	tooltip.classList.toggle("tooltip_active");
-	event.preventDefault()
+		tooltip.style.top = `${positiontop + height}px`;
+		tooltip.style.left = `${positionleft}px`;
+		hastooltip.appendChild(tooltip);
+		event.preventDefault();
+	});
 });
