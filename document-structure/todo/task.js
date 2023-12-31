e0169660c0dba7b3781272022a9691c9d947c065
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2023-12-31 08:11:32 +0300

    список дел 2 попытка

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -7,26 +7,19 @@ button.addEventListener('click', event => {
 	const input = document.querySelector(".tasks__input");
 	let taskname = input.value;
 
-	const divtask = document.createElement("div");
-	divtask.setAttribute("class", "task");
-
-	const divtasktitle = document.createElement("div");
-	divtasktitle.setAttribute("class", "task__title");
-	divtasktitle.textContent = taskname;
-	divtask.appendChild(divtasktitle);
-	const adivtask = document.createElement("a");
-	adivtask.setAttribute("href", "#");
-	adivtask.setAttribute("class", "task__remove");
-	adivtask.innerHTML = '&times';
-	divtask.appendChild(adivtask);
-	taskslist.appendChild(divtask);
-	form.reset();
-
-	const removes = document.querySelectorAll(".task__remove");
-	removes.forEach(remove =>
-		remove.addEventListener('click', event => {
-			event.preventDefault();
-			console.log(event.target.closest(".task"));
-			event.target.closest(".task").remove();
-		}))
+	if (taskname.trim()) {
+		taskslist.insertAdjacentHTML('afterbegin', `
+<div class="task">
+  <div class="task__title">
+    ${taskname}
+  </div>
+  <a href="#" class="task__remove">&times;</a>
+</div>`);
+		form.reset();
+	}
+	const remove = taskslist.querySelector(".task__remove");
+	remove.addEventListener('click', event => {
+		event.preventDefault();
+		event.target.closest(".task").remove();
+	});
 });
