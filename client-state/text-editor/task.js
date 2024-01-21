b32a7874e295e02acd5e05288c086a66c52801ec
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-01-21 12:28:03 +0300

    1 попытка

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,5 @@
+const editor = document.getElementById('editor');
+editor.addEventListener('input', () => {
+	localStorage.setItem('text', editor.value);
+});
+editor.value = localStorage.getItem('text');
